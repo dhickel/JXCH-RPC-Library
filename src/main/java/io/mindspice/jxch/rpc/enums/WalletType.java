e5ce43deb23aad2dc07e83307e9d09616b503d7d
mindspice <admin@mindspice.io>
2023-10-20 08:01:06 -0400

    wallet types

--- src/main/java/io/mindspice/jxch/rpc/enums/WalletType.java
+++ src/main/java/io/mindspice/jxch/rpc/enums/WalletType.java
@@ -1,8 +1,25 @@
 package io.mindspice.jxch.rpc.enums;
 
 public enum WalletType {
-    CAT_WALLET,
-    DID_WALLET,
-    NFT_WALLET,
-    POOL_WALLET
+
+    STANDARD_WALLET(0),
+    ATOMIC_SWAP(2),
+    AUTHORIZED_PAYEE(3),
+    MULTI_SIG(4),
+    CUSTODY(5),
+    CAT(6),
+    RECOVERABLE(7),
+    DISTRIBUTED_ID(8),
+    POOLING_WALLET(9),
+    NFT(10),
+    DATA_LAYER(11),
+    DATA_LAYER_OFFER(12),
+    VC(13),
+    CRCAT(14);
+
+    public final int value;
+
+    WalletType(int value) {
+        this.value = value;
+    }
 }
